252821189aec1b9d68cfe7cfab8b76aa5c5a63d8
Buu Nhut <nhut.nta@gmail.com>
2025-08-08 20:57:19 +0700

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,17 +5,20 @@ const Home = () => {
   return (
     <main id="main">
       <h1>Bách hóa Hân Hân</h1>
-      <p>Phục vụ tận răng 😄</p>
+      <p>Giao hàng tận nơi, tiện lợi và an toàn 😄</p>
       <p className="zalo-link">
         <a
-          href="https://zalo.me/0919317710"
+          href="https://zalo.me/3569852425617577963"
           target="_blank"
           rel="noopener noreferrer"
           className="zalo-link"
         >
-          Zalo: 0919 317710
+          Zalo: 0919 317 710
         </a>
       </p>
+      <div className="zalo-qr">
+        <img src="./img/qrBhhh.jpg" alt="" />
+      </div>
       <button>Đăng ký thành viên</button>
     </main>
   );
