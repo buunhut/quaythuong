63a98be7c3196a974e163d7d1ecba2b8d0684725
Buu Nhut <nhut.nta@gmail.com>
2025-08-21 10:33:45 +0700

    khtt

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,50 +1,40 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./home.scss";
-import { NavLink, useParams } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
-  const getUserId = () => {
-    let userId = localStorage.getItem("user_id");
-    if (!userId) {
-      userId = uuidv4();
-      localStorage.setItem("user_id", userId);
-    }
-    // console.log(userId);
-  };
-
-  getUserId();
-
-  const path = window.location.pathname;
-
   return (
-    <main id="main">
-      <h1>Bách hóa Hân Hân</h1>
-      <p>
-        Giao hàng tận nơi, tiện lợi và an toàn{" "}
-        <NavLink to={"quay-thuong"}>😄</NavLink>
-      </p>
-      <p className="zalo-link">
-        <a
-          href="https://zalo.me/3569852425617577963"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="zalo-link"
-        >
-          Zalo: 0919 317 710
-        </a>
-      </p>
-      <div className="zalo-qr">
-        <img src="./img/qrBhhh.jpg" alt="" />
-      </div>
-      <button type="button">
-        {path === "/winner" ? (
+    <div className="body">
+      <main id="main">
+        <h1>Bách hóa Hân Hân</h1>
+        <p>
+          Giao hàng tận nơi, tiện lợi và an toàn{" "}
+          <NavLink to={"/quay-thuong"}>😄</NavLink>
+        </p>
+        <p className="zalo-link">
+          <a
+            href="https://zalo.me/3569852425617577963"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="zalo-link"
+          >
+            Zalo: 0919 317 710
+          </a>
+        </p>
+        <div className="zalo-qr">
+          <img src="./img/qrBhhh.jpg" alt="" />
+        </div>
+        <button type="button">
+          {/* {path === "/winner" ? (
           <NavLink to={"/"}>🏠 Trang chủ</NavLink>
         ) : (
           <NavLink to={"/winner"}>👀 Khách hàng trúng thưởng</NavLink>
-        )}
-      </button>
-    </main>
+        )} */}
+
+          <NavLink to={"/winner"}>👀 Khách hàng trúng thưởng</NavLink>
+        </button>
+      </main>
+    </div>
   );
 };
 
--- src/pages/Winner.js
+++ src/pages/Winner.js
@@ -1,35 +1,59 @@
 import React from "react";
-import Home from "./Home";
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 
 const Winner = () => {
   const { listWinner } = useSelector((state) => state.dataSlice);
   return (
-    <div id="winner">
-      <div className="left">
-        <Home />
-      </div>
-      <div className="content">
-        <h2>KHÁCH HÀNG TRÚNG THƯỜNG</h2>
-        <table>
-          <thead>
-            <tr>
-              <td>Ngày</td>
-              <td>Tên Khách Hàng</td>
-            </tr>
-          </thead>
-          <tbody>
-            {listWinner?.map((item, index) => {
-              const { ngay, ten } = item;
-              return (
-                <tr key={index}>
-                  <td>{ngay}</td>
-                  <td>{ten}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+    <div className="body">
+      <div id="winner">
+        <div className="left">
+          <main id="main">
+            <h1>Bách hóa Hân Hân</h1>
+            <p>
+              Giao hàng tận nơi, tiện lợi và an toàn{" "}
+              <NavLink to={"/quay-thuong"}>😄</NavLink>
+            </p>
+            <p className="zalo-link">
+              <a
+                href="https://zalo.me/3569852425617577963"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="zalo-link"
+              >
+                Zalo: 0919 317 710
+              </a>
+            </p>
+            <div className="zalo-qr">
+              <img src="./img/qrBhhh.jpg" alt="" />
+            </div>
+            <button type="button">
+              <NavLink to={"/"}>🏠 Trang chủ</NavLink>
+            </button>
+          </main>
+        </div>
+        <div className="content">
+          <h2>KHÁCH HÀNG TRÚNG THƯỜNG</h2>
+          <table>
+            <thead>
+              <tr>
+                <td>Ngày</td>
+                <td>Tên Khách Hàng</td>
+              </tr>
+            </thead>
+            <tbody>
+              {listWinner?.map((item, index) => {
+                const { ngay, ten } = item;
+                return (
+                  <tr key={index}>
+                    <td>{ngay}</td>
+                    <td>{ten}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
